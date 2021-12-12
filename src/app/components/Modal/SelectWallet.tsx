a03ec94688844e76f97b0431b96c19021bd176f4
Alex <web.senior.dev@gmail.com>
2021-12-11 19:00:55 -0700

    update the theme modal.

--- src/app/components/Modal/SelectWallet.tsx
+++ src/app/components/Modal/SelectWallet.tsx
@@ -2,12 +2,15 @@ import { makeStyles, Theme } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 import MetaMask from '../../assets/Popup/metamask.png';
 import Trezor from '../../assets/Popup/trezor.png';
+import TrezorBlack from '../../assets/Popup/trezor_black.png';
 import CoinbaseWallet from '../../assets/Popup/coinbase_wallet.png';
 import Autherium from '../../assets/Popup/autherium.png';
 import GnezisSafe from '../../assets/Popup/gnezis_safe.png';
+import GnezisSafeBlack from '../../assets/Popup/gnezis_black.png';
 import KeyStone from '../../assets/Popup/keystone.png';
 import Lattiz from '../../assets/Popup/lattiz.png';
 import Ledgez from '../../assets/Popup/ledgez.png';
+import LedgezBlack from '../../assets/Popup/ledgez_black.png';
 import Opera from '../../assets/Popup/opera.png';
 import WalletConnect from '../../assets/Popup/walletconnect.png';
 
@@ -15,34 +18,34 @@ const useStyles = makeStyles((theme: Theme) => ({
   root: {
     flexGrow: 1,
     margin: 'auto',
-    color: '#fff',
-    boxShadow: '0px 0px 10px 10px #0c0f16'
+    color: theme.palette.error.light,
+    boxShadow: `0px 0px 10px 10px ${theme.palette.info.main}`
   },
-  //common class
   marginLeft10: {marginLeft: 10},
   verticalAlignMiddle: {verticalAlign: 'middle'},
   wallet_header_content: {
     color: '#919398', 
     textAlign: 'left'
   },
-  //component class
   modal_header: {
-    background: '#2c333f',
+    background: theme.palette.background.paper,
     textAlign: 'left',
     borderTopRightRadius: 5,
     borderTopLeftRadius: 5,
+    borderBottom: `1px solid ${theme.palette.info.main}`,
   },
   modal_header_title: {
     padding: '25px 40px',
     display: 'block',
     fontSize: 20,
-    fontFamily: 'CerebriSansPro-ExtraBold'
+    fontFamily: 'CerebriSansPro-ExtraBold',
+    textTransform: 'uppercase'
   },
-  project_info_1: {
-    background: '#1c222d',
-    border: '1px solid #2f3540',
-    padding: '25px 40px',
-  },
+  // project_info_1: {
+  //   background: '#1c222d',
+  //   border: '1px solid #2f3540',
+  //   padding: '25px 40px',
+  // },
   icon_size: {
     width: 35,
     height: 35
@@ -50,12 +53,13 @@ const useStyles = makeStyles((theme: Theme) => ({
   select_header: {
     width: '100%',
     textAlign: 'left',
-     marginBottom: 20},
+     marginBottom: 20
+  },
   wallet_info_desc: {
     borderBottomRightRadius: 5, 
     borderBottomLeftRadius: 5,
-    background: '#1c222d',
-    border: '1px solid #2f3540',
+    background: theme.palette.background.paper,
+    borderBottom: `1px solid ${theme.palette.info.main}`,
     padding: '25px 40px'
   },
   wallet_header_title: {
@@ -64,8 +68,8 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   wallet_image_content: {
     textAlign: 'left',
-    background: '#1c222d',
-    border: '1px solid #2f3540',
+    background: theme.palette.background.paper,
+    borderBottom: `1px solid ${theme.palette.info.main}`,
     padding: '25px 40px',
     fontFamily: 'CerebriSansPro-ExtraBold'
   },
@@ -78,7 +82,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     fontSize: 12,
     borderRadius: 10,
     '&:hover': {
-      boxShadow: '0 0 10px 10px #555'
+      boxShadow: `0 0 10px 10px ${theme.palette.info.main}`
     }
   }
 }));
@@ -101,7 +105,7 @@ export default function SelectWallet({connect}: connectType) {
           <img alt='metamask' src={MetaMask} className={classes.verticalAlignMiddle}></img><span className={classes.marginLeft10}>Metamask</span>
         </Grid>
         <Grid item xs={12} sm={6} md={3} className={classes.wallets}>
-          <img className={classes.verticalAlignMiddle} alt='Trezor' src={Trezor}></img><span  className={classes.marginLeft10}>Trezor</span>
+          <img className={classes.verticalAlignMiddle} alt='Trezor' src={ localStorage.getItem('appTheme') === 'lightTheme' ? TrezorBlack : Trezor}></img><span  className={classes.marginLeft10}>Trezor</span>
         </Grid>
         <Grid item xs={12} sm={6} md={3} className={classes.wallets}>
           <img alt='CoinbaseWallet' src={CoinbaseWallet} className={classes.verticalAlignMiddle}></img><span  className={classes.marginLeft10}>Coinbase Wallet</span>
@@ -110,7 +114,7 @@ export default function SelectWallet({connect}: connectType) {
           <img alt='Opera' src={Opera} className={classes.verticalAlignMiddle}></img><span  className={classes.marginLeft10}>Opera</span>
         </Grid>
         <Grid item xs={12} sm={6} md={3} className={classes.wallets}>
-          <img alt='Ledgez' src={Ledgez} className={classes.verticalAlignMiddle}></img><span  className={classes.marginLeft10}>Ledgez</span>
+          <img alt='Ledgez' src={ localStorage.getItem('appTheme') === 'lightTheme' ? LedgezBlack : Ledgez} className={classes.verticalAlignMiddle}></img><span  className={classes.marginLeft10}>Ledgez</span>
         </Grid>
         <Grid item xs={12} sm={6} md={3} className={classes.wallets}>
           <img alt='WalletConnect' src={WalletConnect} className={classes.verticalAlignMiddle}></img><span  className={classes.marginLeft10}>Walletconnect</span>
@@ -122,7 +126,7 @@ export default function SelectWallet({connect}: connectType) {
           <img alt='Lattiz' src={Lattiz} className={classes.verticalAlignMiddle}></img><span  className={classes.marginLeft10}>Lattiz</span>
         </Grid>
         <Grid item xs={12} sm={6} md={3} className={classes.wallets}>
-          <img alt='GnezisSafe' src={GnezisSafe} className={classes.verticalAlignMiddle}></img><span  className={classes.marginLeft10}>Gnezis safe</span>
+          <img alt='GnezisSafe' src={ localStorage.getItem('appTheme') === 'lightTheme' ? GnezisSafeBlack : GnezisSafe} className={classes.verticalAlignMiddle}></img><span  className={classes.marginLeft10}>Gnezis safe</span>
         </Grid>
         <Grid item xs={12} sm={6} md={3} className={classes.wallets}>
           <img alt='Autherium' src={Autherium} className={classes.verticalAlignMiddle}></img><span  className={classes.marginLeft10}>Autherium</span>
