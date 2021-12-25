e76cd5ace21ec41d454f7adf424c690df67232a6
Gogs <gogs@fake.local>
2021-12-25 11:29:39 +0900

    Warning Removed: React Restrict Mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import ThemeProvider from "./app/theme/ThemeProvider";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { store } from './app/store/store'
+import { Provider } from 'react-redux'
+import * as serviceWorker from './serviceWorker'
+import ThemeProvider from './app/theme/ThemeProvider'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
